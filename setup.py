9e706249c8ec3b1c564e7b6c8f49160a98fa168c
Rajas Sambhare <sambhare@users.noreply.github.com>
2020-06-17 09:36:47 -0400

    Updated requirements to libdfx >= 4.4

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfxdemo',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'dfx-apiv2-client @ https://github.com/nuralogix/dfx-apiv2-client-py/tarball/master',
-        'libdfx>=4',
+        'libdfx>=4.4',
         'cmake',
         'dlib>=19',
         'opencv-python>=4',
