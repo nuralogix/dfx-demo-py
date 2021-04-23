a46733965594a8531c2ebebb25c36a9d22ed5814
Rajas Sambhare <sambhare@users.noreply.github.com>
2021-04-23 13:55:19 -0400

    Avoid dlib 19.22 since it has issues

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfxdemo',
-    version='0.4.1',
+    version='0.4.2',
     packages=find_packages(),
     install_requires=[
         'dfx-apiv2-client @ https://github.com/nuralogix/dfx-apiv2-client-py/tarball/master',
         'libdfx>=4.4',
-        'dlib>=19',
+        'dlib>=19,!=19.22',
         'opencv-python>=4',
         'pymediainfo',
     ],
