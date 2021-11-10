9b6759fbcb631534dafdfca33e562c98e7bff45d
Rajas Sambhare <sambhare@users.noreply.github.com>
2021-11-10 14:13:01 -0500

    Bumped version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfxdemo',
-    version='0.9.0',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'dfx-apiv2-client @ https://github.com/nuralogix/dfx-apiv2-client-py/tarball/master',
