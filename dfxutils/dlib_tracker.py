993788fdea19664681ac7cd1e8656dc6485640be
KitsuELP <eric.j.levy@gmail.com>
2022-07-29 13:24:59 -0400

    added kalman and bollinger band filtering

--- dfxutils/dlib_tracker.py
+++ dfxutils/dlib_tracker.py
@@ -1,21 +1,48 @@
+#This file updated on 21 jul 2022 to include bollinger filtering
+#Updated on 25 jul 2022 to include Kalman filtering
+#updated 28 jul 2022 to improve kalman filtering
+#updated 29 jul 2022 to allow for filter type choice from call
+
+
 import multiprocessing as mp
 import os
 import queue
 from statistics import median
+from statistics import pstdev 
+from statistics import mean
+from numpy import dot
+import math
 
 import dlib
 import numpy as np
 
 
 class DlibTracker():
-    def __init__(self, face_detect_strategy=None):
+    def __init__(self, smoothing=None, face_detect_strategy=None):
         try:
             self._detect_proc = None
             model_path = os.path.join(os.getcwd(), "res", "shape_predictor_68_face_landmarks.dat")
 
             self._face_detector = dlib.get_frontal_face_detector()
             self._pose_estimator = dlib.shape_predictor(model_path)
+            self._smoothingType = smoothing
             self._smoothed = {pt: ([], []) for pt in DlibTracker._dlib2mpeg4}
+            self._bollinger = { pt: {
+                                     'upperBnd' : ([],[]),
+                                     'lowerBnd' : ([],[]),
+                                     'window'   : ([],[])} for pt in DlibTracker._dlib2mpeg4}
+            self._kalmanFrames = 0
+            self._procNoiseCov = 2.8
+            self._sampleNo = 0
+            self._kalman = { pt: {
+                                'filtered'        : (0,0),
+                                'weightedDist'    : (0,0),
+                                'P'               : (0.72,0.72),
+                                'procNoiseCov'    : (0.72,0.72),
+                                'measureNoiseCov' : (0.04,0.04),
+                                'dist'            : (0,0),
+                                'weightedDist'    : (0,0),
+                                'prevPt'          : (0,0)} for pt in DlibTracker._dlib2mpeg4}
             if face_detect_strategy is None:
                 face_detect_strategy = "smart"
             self._fd_fast = face_detect_strategy != "brute"
@@ -81,8 +108,8 @@ class DlibTracker():
                 newx = face_points.part(j).x + x if face_points.part(j).x + x > 0 else 0
                 newy = face_points.part(j).y + y if face_points.part(j).y + y > 0 else 0
                 pointname = DlibTracker._dlib2mpeg4[j]
-                smoothedx, smoothedy = self._smoothPoints(newx, newy, pointname)
-                points[pointname] = {"x": smoothedx, "y": smoothedy, "valid": True, "estimated": True, "quality": 1.0}
+                smoothedx, smoothedy = self._smoothPoints( newx, newy, pointname )
+                points[pointname] = {"x": smoothedx, "y": smoothedy, "valid": True, "estimated": True, "quality": 1.0} #update the pts dict
             faces[str(i)] = {
                 "id": str(i + 1),
                 "rect.x": rect.left() + x if rect.left() + x > 0 else 0,
@@ -103,9 +130,18 @@ class DlibTracker():
             self._work_queue.cancel_join_thread()
             self._results_queue.cancel_join_thread()
 
-    def _smoothPoints(self, newx, newy, pointname, framesToSmooth=10):
-        xs, ys = self._smoothed[pointname]
-        xs.append(newx)
+    def _smoothPoints( self, newx, newy, pointname ):
+        if self._smoothingType =="median":
+            filteredx,filteredy = self._medianFilter( newx, newy, pointname )
+        elif self._smoothingType == "kalman":
+            filteredx, filteredy = self._kalmanFilter( newx, newy, pointname )
+        elif self._smoothingType =="bollinger":
+            filteredx,filteredy = self._bollingerFilter( newx, newy, pointname )
+        return filteredx, filteredy
+
+    def _medianFilter(self, newx, newy, pointname, framesToSmooth=10):
+        xs, ys = self._smoothed[pointname] #get buffer dict by pointname
+        xs.append(newx)                    #add new pts to end of buffer
         ys.append(newy)
         if len(xs) > framesToSmooth:
             self._smoothed[pointname] = xs[1:], ys[1:]
@@ -113,6 +149,149 @@ class DlibTracker():
         smoothedy = median(ys)
 
         return smoothedx, smoothedy
+
+    def _frameCount( self ):
+        #probably a better implementation, using this for now to track how many frames have gone by
+        if self._sampleNo % 68 == 0:
+            self._kalmanFrames +=1
+
+    def _kalmanFilter( self, newx, newy, pointname, A= 1.0, C = 1.0 ):
+        '''
+        the procNoiseCov constant determines the ratio of tracking/filtering
+        a high procNoiseCov will allow the filter to track very fast with little delay, but many outliers
+        a low procNoiseCov will completely eliminate outliers but track the subject poorly
+        a good balanced between tracking/filtering is procNoiseCov = 1.4
+        if the subject is far from the prediction, the noise cov should be raised to allow the prediction to catch up
+        if the subject is near the prediction, the noise cov should be lowered to eliminate outliers
+        '''
+        procNoiseCov = self._procNoiseCov
+
+        self._sampleNo +=1 #keep track of number of samples, in dlib 68pts are sampled per frame
+        self._frameCount() #update which frame we're on, every 68 frames +=1
+
+        eye = np.identity(1)
+
+        filtered = self._kalman[pointname]['filtered']                # pull the pt arrays
+        weightedDist = self._kalman[pointname]['weightedDist']
+        measureNoiseCov = self._kalman[pointname]['measureNoiseCov']
+        P = self._kalman[pointname]['P']
+        prevPt = self._kalman[pointname]['prevPt']
+
+        xFiltered,yFiltered = filtered[0],filtered[1]                 # split the touples
+        xWeighted, yWeighted = weightedDist[0], weightedDist[1]
+        xNoiseCov, yNoiseCov = measureNoiseCov[0], measureNoiseCov[1]
+        xP, yP = P[0], P[1]
+        xPrev,yPrev = prevPt[0], prevPt[1]
+
+        if xPrev == 0:                                                # if this is the first pt, need to initialise
+            xPrev,yPrev = newx, newy
+            self._kalman[pointname]['prevPt'] = (xPrev,yPrev)         # initialise the dicts
+            self._kalman[pointname]['filtered'] = (xPrev,yPrev)
+            return xPrev,yPrev
+
+        #based prediction, adjusted by proc covariance; if high subject is moving, if low subject is still
+        xP, yP = float(dot( A, xP ) + procNoiseCov), float(dot( A, yP ) + procNoiseCov)
+        
+        #innovation step
+        xDist, yDist = (newx - C*xPrev), (newy - C*yPrev)
+        xErr,yErr = ( dot( C, xP ) + xNoiseCov ),( dot( C, yP ) + yNoiseCov )
+        xApprox,yApprox = ( abs(xDist)/xErr ), ( abs(yDist)/yErr )
+        xWeighted,yWeighted = ( 1/(1+(math.exp(-xApprox) + 0.1)) ), ( 1/(1+(math.exp(-yApprox) + 0.1)) )
+        xNoiseCov, yNoiseCov = [4*xWeighted], [4*yWeighted]
+
+        #calculate new Kalman Gain
+        xK, yK = ( dot( xP, dot(1, 1/xErr) ) ), ( dot( yP, dot(1, 1/yErr) ) )
+        #update step
+        xFiltered, yFiltered = (float(xPrev + dot(xK, dot(xDist,xK)))), (float(yPrev + dot(yK, dot(yDist,yK))))
+        xP,yP = ( int(dot(eye - dot(xK,C), xP))), ( int(dot(eye - dot(yK,C), yP)))
+
+        #on every other frame update the proc covariance coeff
+        if self._kalmanFrames % 2 == 0:
+            self._updateProcNoise()
+
+
+        self._kalman[pointname]['filtered'] = ( xFiltered,yFiltered )
+        self._kalman[pointname]['prevPt'] = ( xFiltered, yFiltered )
+        self._kalman[pointname]['weightedDist'] = ( xWeighted, yWeighted )
+        self._kalman[pointname]['measureNoiseCov'] = ( xNoiseCov, yNoiseCov )
+        self._kalman[pointname]['P'] = ( xP,yP )
+        self._kalman[pointname]['dist'] = ( xDist, yDist )
+
+        return xFiltered,yFiltered
+
+    def _updateProcNoise( self, scalingSpeed = 1, vScale = 0.81, vShift = 1.3, hScale = 1.7 ):
+        #gets mean of current distance from all points in prediction vs actual
+
+        '''
+        hScale value sets the expected average distance
+        vScale value sets where the covariance should be at the expected average
+        scalingSpeed value sets how aggressively the output approaches the bounds
+        vScale sets the height of the bounds (be wary of allowing for negative covariance)
+        '''
+        xDist, yDist = 0,0
+        for i,pts in enumerate( self._kalman ):
+            xDist,yDist = ( xDist + self._kalman[pts]['dist'][0] ), ( yDist + self._kalman[pts]['dist'][1] )
+
+        xDist, yDist = ( xDist/(i+1) ),( yDist/(i+1) )                           # get the mean distance
+        currDist = math.sqrt( xDist**2 + yDist**2 )                              # current distance modulus
+                                                                                 # bound coeff by atan function
+        newCoeff = ( np.arctan( scalingSpeed*currDist - hScale))/vScale + vShift
+                                                                                 # update the new covariance
+        self._procNoiseCov = newCoeff
+                                                                                 # debug tracing
+        '''
+        print((currDist,newCoeff))
+        '''
+
+    def _bollingerFilter( self, newx, newy, pointname, bound = 0.9, buffer = 10 ):
+        #pull the pt arrays
+        xWindow, yWindow = self._bollinger[pointname]['window']                 # recover the window
+        xLow, yLow = self._bollinger[pointname]['lowerBnd']                     # recover the previous lo bounds
+        xHi, yHi = self._bollinger[pointname]['upperBnd']                       # Recover the previous hi bounds
+
+        #update the window for ten frames
+        if len( xWindow ) < buffer:
+            xWindow.append( newx )
+            yWindow.append( newy )
+            self._bollinger[pointname]['window'] = xWindow, yWindow
+            return xWindow[-1], yWindow[-1]
+
+        # on the tenth frame build the bounds
+        if len( xWindow ) == buffer:
+            xWindow, yWindow = ( xWindow[:] + [newx] ), ( yWindow[:] + [newy] )  # append the queue
+            xStDev, yStdDev = pstdev( xWindow ), pstdev( yWindow )               # Get the population Standard Dev
+            xAvg, yAvg = mean( xWindow ), mean( yWindow )                        # get the mean of each window
+            xLow, yLow = ( xAvg - bound * xStDev ), (yAvg - bound * yStdDev )    # set the lower bounds of x and y
+            xHi, yHi = ( xAvg + bound * xStDev ) , (yAvg + bound * yStdDev )     # set the upper bound
+            self._bollinger[pointname]['window'] = xWindow, yWindow
+            self._bollinger[pointname]['lowerBnd'] = xLow,yLow
+            self._bollinger[pointname]['upperBnd'] = xHi,yHi
+            return xWindow[-1], yWindow[-1]                                      # return raw pts
+
+        xPrev, yPrev = xWindow[-1], yWindow[-1]                                  # default if no bound is needed
+        if xPrev > xHi:                                                          # Bound the points based on previous inputs
+            xPrev = xHi
+        if xPrev < xLow:
+            xPrev = xLow
+        if yPrev > yHi:
+            yPrev = yHi
+        if yPrev < yLow:
+            yPrev = yLow
+
+        #then update points
+        xWindow, yWindow = ( xWindow[1:] + [newx] ), ( yWindow[1:] + [newy] )    # pop the queue
+        xStDev, yStdDev = pstdev( xWindow ), pstdev( yWindow )                   # Get the population Standard Dev
+        xAvg, yAvg = mean( xWindow ), mean( yWindow )                            # get the mean of each window
+        xLow, yLow = ( xAvg - bound * xStDev ), (yAvg - bound * yStdDev )        # set the lower bounds of x and y
+        xHi, yHi = ( xAvg + bound * xStDev ) , (yAvg + bound * yStdDev )         # set the upper bound
+        #stuff back into point dicts
+        self._bollinger[pointname]['window'] = xWindow, yWindow
+        self._bollinger[pointname]['lowerBnd'] = xLow, yLow
+        self._bollinger[pointname]['upperBnd'] = xHi, yHi
+        
+        return xPrev, yPrev
+        #return (0.8* xPrev + 0.2 * xAvg ),( 0.8* yPrev + 0.2*yAvg) #Return the filtered pts
+
 
     # TODO check this
     def _sanitizeRoi(self, shape, rect=None):
