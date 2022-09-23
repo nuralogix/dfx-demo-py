9252e05abdc898b29a930849cf4c5e6710422fb5
Rajas Sambhare <sambhare@users.noreply.github.com>
2022-09-23 13:19:52 -0400

    Switch back to using PyPI for dfx-apiv2-client

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.16.0',
     packages=find_packages(),
     install_requires=[
-        'dfx-apiv2-client @ https://github.com/nuralogix/dfx-apiv2-client-py/tarball/remove-protos',
+        'dfx-apiv2-client>0.10,<=0.11',
         'libdfx>=4.9',
         'opencv-python>=4.5,<4.7'
     ],
