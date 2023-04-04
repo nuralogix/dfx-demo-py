ba3e5f60cd8243c571b4e6e4dc91335f5218fc74
Rajas Sambhare <sambhare@users.noreply.github.com>
2023-04-04 17:19:27 -0400

    Allow opencv 4.7.x for compat with mediapipe

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfxdemo',
-    version='0.20.0',
+    version='0.20.1',
     packages=find_packages(),
     install_requires=[
         'dfx-apiv2-client>=0.13,<0.14',
         'libdfx>=4.9',
-        'opencv-python>=4.5,<4.7',
+        'opencv-python>=4.5,<4.8',
     ],
     extras_require={
         "dlib": ["dlib>=19,!=19.22,!=19.22.1"],
