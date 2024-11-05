9363af496a3ebc6059eee0076cdb41facd8808a2
Rajas Sambhare <sambhare@users.noreply.github.com>
2024-11-05 10:56:04 -0500

    Update to libdfxpython 4.14.5

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfxdemo',
-    version='0.23.0',
+    version='0.23.1',
     packages=find_packages(),
     install_requires=[
         'dfx-apiv2-client>=0.14,<0.15',
-        'libdfx>=4.12',
+        'libdfx>=4.14',
     ],
     extras_require={
         "dlib": ["dlib>=19,!=19.22,!=19.22.1", "opencv-python>=4.6,<5"],
